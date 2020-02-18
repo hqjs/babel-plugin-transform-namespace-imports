c0ec8aca5da68c7713c460bbca9cac27c77c59f5
hqjs <hqjs.org@gmail.com>
2020-02-18 22:47:29 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# babel-plugin-transform-namespace-imports
+# https://hqjs.org
 Transform namespace imports into default imports
+
+# Installation
+```sh
+npm install hqjs@babel-plugin-transform-namespace-imports
+```
+
+# Usage
+```json
+{
+  "plugins": [["hqjs@babel-plugin-transform-namespace-imports", {
+    "packages": ["React", "ReactDOM"]
+  }]]
+}
+```
+
+# Transformation
+Transforms namespace imports into default imports to meet specification requirements
+
+```js
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+```
+we will obtain
+```js
+import React from 'react';
+import ReactDOM from 'react-dom';
+```
---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = function({ types: t }) {
+  return {
+    visitor: {
+      ImportDeclaration(nodePath) {
+        const { packages } = stats.opts;
+        const { specifiers } = nodePath.node;
+
+        const spcs = [];
+        for (const spec of specifiers) {
+          console.log(t.isImportNamespaceSpecifier(spec));
+          if (t.isImportNamespaceSpecifier(spec) && packages.includes(spec.local.name)) {
+          	spcs.push(t.importDefaultSpecifier(spec.local));
+          } else {
+            spcs.push(spec);
+          }
+        }
+        console.log(spcs);
+        nodePath.node.specifiers = spcs;
+      },
+    },
+  };
+};
