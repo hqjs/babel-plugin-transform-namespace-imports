ff32b8e719480e677b7a202326210796554ee917
hqjs <hqjs.org@gmail.com>
2020-02-21 20:10:28 +0200

    Add runtime transformation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # https://hqjs.org
-Transform namespace imports into default imports
+Transform namespace imports into default imports when required
 
 # Installation
 ```sh
@@ -10,20 +10,27 @@ npm install hqjs@babel-plugin-transform-namespace-imports
 ```json
 {
   "plugins": [["hqjs@babel-plugin-transform-namespace-imports", {
-    "packages": ["React", "ReactDOM"]
+    "include": ["react-dom"],
+    "exclude": ["lodash"]
   }]]
 }
 ```
 
 # Transformation
-Transforms namespace imports into default imports to meet specification requirements
+Transforms namespace imports into default imports to meet specification requirements. Packages from `include` will be transformed to default import without runtime checking. Packages from `exclude` won't be transformed. Rest packages will be checked and transformed in runtime.
 
 ```js
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as _ from 'lodash';
+import * as R from 'ramda';
 ```
 we will obtain
 ```js
-import React from 'react';
+import * as _ref from 'react';
+const React = Object.keys(_ref).length === 1 && _ref.default ? _ref.default : _ref;
 import ReactDOM from 'react-dom';
+import * as _ from 'lodash';
+import * as _ref1 from 'ramda';
+const R = Object.keys(_ref1).length === 1 && _ref1.default ? _ref1.default : _ref1;
 ```
--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 module.exports = function({ types: t }) {
+  const objectKeys = t.memberExpression(
+    t.identifier('Object'),
+    t.identifier('keys')
+  );
+  const length = t.identifier('length');
+  const one = t.numericLiteral(1);
+
   return {
     visitor: {
       ImportDeclaration(nodePath, stats) {
-        const { packages } = stats.opts;
-        const { specifiers } = nodePath.node;
+        const { include = [], exclude = [] } = stats.opts;
+        const { specifiers, source } = nodePath.node;
 
         const spcs = [];
         for (const spec of specifiers) {
-          if (t.isImportNamespaceSpecifier(spec) && packages.includes(spec.local.name)) {
-          	spcs.push(t.importDefaultSpecifier(spec.local));
+          if (t.isImportNamespaceSpecifier(spec) && include.includes(source.value)) {
+            spcs.push(t.importDefaultSpecifier(spec.local));
+          } else if (t.isImportNamespaceSpecifier(spec) && !exclude.includes(source.value)) {
+            const name = nodePath.scope.generateUidIdentifierBasedOnNode('namespace');
+            spcs.push(t.importNamespaceSpecifier(name));
+            const exportDefault = t.memberExpression(
+              name,
+              t.identifier('default')
+            );
+            const assignment = t.variableDeclaration(
+              'const',
+              [t.variableDeclarator(
+                spec.local,
+                t.conditionalExpression(
+                  t.logicalExpression(
+                    '&&',
+                    t.binaryExpression(
+                      '===',
+                      t.memberExpression(
+                        t.callExpression(
+                          objectKeys,
+                          [name]
+                        ),
+                        length
+                      ),
+                      one
+                    ),
+                    exportDefault
+                  ),
+                  exportDefault,
+                  name
+                )
+              )]
+            );
+          nodePath.insertAfter(assignment);
           } else {
             spcs.push(spec);
           }
